e0e75c637091a9f40810a6c23c5a8fc0b33151a5
guilh <guilh@DESKTOP-U3T69CD>
2025-01-21 18:09:47 -0300

    Adicionar arquivos de projeto.

---
+++ surpresa/Program.cs
@@ -0,0 +1,25 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool iloveu = true;
+
+            while (iloveu != false)
+            { 
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("i love u");
+            }
+
+
+        }
+    }
+}
